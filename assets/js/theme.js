df952adc0b47d9b69efb387dba9eced24062f086
Megh Shukla <work.meghshukla+1@gmail.com>
2025-03-09 20:25:16 +0100

    Updating TIC-TAC

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -44,24 +44,3 @@ function getTheme() {
 function setTheme(value) {
   document.documentElement.setAttribute(THEME_ATTR, value);
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-    const detailsElements = document.querySelectorAll("details");
-
-    detailsElements.forEach((details) => {
-      details.addEventListener("toggle", (event) => {
-        if (details.open) {
-          details.style.height = details.scrollHeight + "px";
-        } else {
-          details.style.height = "0px";
-        }
-      });
-
-      // Set initial height
-      if (details.open) {
-        details.style.height = details.scrollHeight + "px";
-      } else {
-        details.style.height = "0px";
-      }
-    });
-});
